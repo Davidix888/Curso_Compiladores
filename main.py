dbdae3b077daeaf84937a099313fa556a1754fb3
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2026-03-09 18:00:42 -0600

    HT02 - Analizador Lexico
    Implemente el reconocimiento de las instrucciones: print, println, for, while, if y else.

---
+++ main.py
@@ -0,0 +1,56 @@
+from html import parser
+from lexico import * 
+from sintactico import *
+
+
+
+def main():
+    #Ejemplo
+    codigo_fuente = """
+    int suma(int a, int b) {
+      return a + b;
+    }
+
+    int main() {
+      int resultado = suma(5, 8);
+      return resultado;}
+    """
+
+    #Ejemplo 2
+    codigo_fuenteDos = """
+    int suma(int a, int b) {
+      int c = a + b;
+      printf(c);
+      return c;
+    }
+    """
+    tokensDos = identificar_tokens(codigo_fuenteDos)
+    tokens = identificar_tokens(codigo_fuente) 
+    arbol_astDos = None
+  
+    #Prueba sintáctica
+    try:
+        print("Iniciando analisis sintactico")
+        parser = Parser(tokens)
+        arbol_ast = parser.parsear()
+        print("Analisis sintactico exitoso")
+    except SyntaxError as e:
+        print(f"Error sintáctico: {e}")
+
+    try:
+        print("Iniciando analisis sintactico")
+        parser = Parser(tokensDos)
+        arbol_astDos = parser.parsear()
+        print("Analisis sintactico exitoso")
+    except SyntaxError as e:
+        print(f"Error sintáctico: {e}")
+
+    print(json.dumps(imprimir_ast(arbol_ast), indent=1))
+    
+    
+    nodoExp = NodoOperacion(NodoNumero(5),'+',NodoNumero(8))
+    print(json.dumps(imprimir_ast(nodoExp), indent=1))
+    if arbol_astDos:
+      print(arbol_astDos.traducirPy())
+
+main()
